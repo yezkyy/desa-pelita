bcf7f5bc419cd9c1a21d4b39f0397bb6a1bd7daa
Rizky <muhammadrizkys2004@gmail.com>
2025-03-01 05:45:17 +0800

    Refactor DatabaseSeeder to call individual seeders for Users, Berita, Kuliner, and Wisata

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Memanggil semua seeder
+        $this->call([
+            UsersTableSeeder::class,
+            BeritaSeeder::class,
+            KulinerSeeder::class,
+            WisataSeeder::class,
         ]);
     }
 }
